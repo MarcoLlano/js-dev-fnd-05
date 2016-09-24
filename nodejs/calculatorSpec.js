5b3b573aed63fb835d4a2aea5e75f99e8df7a16c
jalatraining <jalatraining@fundacion-jala.local>
2016-09-24 11:49:05 -0400

    Adding new changes

--- nodejs/calculatorSpec.js
+++ nodejs/calculatorSpec.js
@@ -1,9 +1,16 @@
 //calculatorSpec, this is the unit test file using jasmine.
 var Calculator = require('./calculator');
 describe('Calculator', function() {
+	
+	//Global variable
+	var calculator;
+	
+	beforeEach(function() {		
+		calculator = new Calculator();
+	});
+	
 	it('should be able to add two numbers', function() {
 		//Test steps
-		var calculator = new Calculator();
 		var expRes = 3;
 		var actRes = calculator.add(1,2);
 		expect(actRes).toBe(expRes);
@@ -11,7 +18,6 @@ describe('Calculator', function() {
 	
 	it('should be able to substract two numbers', function() {
 		//Test steps
-		var calculator = new Calculator();
 		var expRes = 3;
 		var actRes = calculator.substract(8,5);
 		expect(actRes).toBe(expRes);
@@ -20,7 +26,6 @@ describe('Calculator', function() {
 	//Multiply
 	it('should be able to multiply two numbers', function() {
 		//Test steps
-		var calculator = new Calculator();
 		var expRes = 15;
 		var actRes = calculator.product(3,5);
 		expect(actRes).toBe(expRes);
@@ -29,15 +34,13 @@ describe('Calculator', function() {
 	//Divide
 	it('should be able to divide two numbers', function() {
 		//Test steps
-		var calculator = new Calculator();
 		var expRes = 15;
 		var actRes = calculator.divide(30,2);
 		expect(actRes).toBe(expRes);
 	});
 	
 	it('should be able to control divide by 0 exception', function() {
-		//Test steps
-		var calculator = new Calculator();		
+		//Test steps	
 		expect(function(){
 			calculator.divide(3, 0);
 		}).toThrow('divide by 0 is error');
