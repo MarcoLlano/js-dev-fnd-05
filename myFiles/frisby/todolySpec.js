941f31765d755f7f83073eba7325f7739f1585ee
jalatraining <veronica.sasha@gmail.com>
2016-10-15 11:13:32 -0400

    Adding new test cases

--- myFiles/frisby/todolySpec.js
+++ myFiles/frisby/todolySpec.js
@@ -1,15 +1,15 @@
 //Todoly spec
-
 var frisby = require('frisby');
 
 frisby.globalSetup({
 	request: {
 		headers: {
-			'Authorization' : 'Basic bGxhbm8ubWFyY29AZ21haWwuY29tOjM2OXhsYnVqMjU='
+			'Authorization' : 'Basic bGxhbm8ubWFyY29AZ21haWwuY29tOjM2OXhsYnVqMjU=',
+			'Content-Type' : 'application/json'
 		}
 	}	
 });
-
+/*
 frisby
 	.create('Should return a specific project')
 	.get('https://todo.ly/api/projects/3355036.json')
@@ -31,5 +31,20 @@ frisby
 frisby
 	.create('Get all projects')
 	.get('https://todo.ly/api/projects.json')
+	.expectStatus(200)
+.toss();
+*/
+/*
+frisby
+	.create('Should create a project with a required value')
+	.post('https://todo.ly/api/projects.json', {Content: "My new project frisby"}, { json: true })
+	.expectJSON({Content: "My new project frisby" })
+	.expectStatus(200)
+.toss();*/
+
+frisby
+	.create('Should not create a project with a required value')
+	.post('https://todo.ly/api/projects.json', {Content: " "}, { json: true })
+	.expectJSON({Content: " " })
 	.expectStatus(200)
 .toss();
